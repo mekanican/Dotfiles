5ff51b0ff6d5d345b7daa983a044768fe4d71fca
nhannghia73 <boommerang73@gmail.com>
2019-06-17 00:11:04 +0700

    second

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long int32;
+typedef unsigned long uint32;
+typedef long long int64;
+typedef unsigned long long uint64;
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+#define st first
+#define nd second
+#define pb push_back
+#define mp make_pair
+#define FOR(i,a,b) for(int i = (a); i < (b); i++)
+#define rFOR(i,a,b) for(int i = (a); i >= (b); i--)
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define all(x) (x).begin(),(x).end()
+#define debug(x,n) FOR(i,0,n) cout << x[i] << " "; cout << endl
+
+// Custom global variable 
+
+
+
+
+// ----------------------
+auto main() -> int
+{
+  #ifdef DEBUG
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+  #endif
+  // Fast I/O
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  // Code starts here ---
+  
+
+  // ---------------------
+  #ifdef DEBUG
+    cerr << endl << "Time:" << double(clock())/CLOCKS_PER_SEC << " secs" << endl;
+  #endif
+}
